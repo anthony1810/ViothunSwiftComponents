d07b74d3f211ed0afbcc91c250471d652e88d250
Quang Tran <quang.tran@appetiser.com.au>
2023-06-18 11:14:08 +0700

    Initial Commit

---
+++ ViothunSwiftUIComponents/ViothunSwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViothunSwiftUIComponentsApp.swift
+//  ViothunSwiftUIComponents
+//
+//  Created by Anthony Tran on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViothunSwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
